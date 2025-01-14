9f0dca11299ad518d80e35b6e31bd3d4e4075e6b
Cameron Brown <moontsukuyomi6@gmail.com>
2025-01-14 05:39:33 +0200

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatValue) {
+  if (repeatValue < 0) {
+    return 'ERROR';
+  }
+  return string.repeat(repeatValue);
 };
-
+ 
 // Do not edit below this line
 module.exports = repeatString;
