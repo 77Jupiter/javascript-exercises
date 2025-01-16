60e82ba6fb4d90db4890c5d86cad423331e05248
Cameron Brown <moontsukuyomi6@gmail.com>
2025-01-16 02:33:13 +0200

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if ((num1 < 0 || num2 < 0) || (num1 % 1 !== 0 || num2 % 1 !== 0) || typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+    return 'ERROR';
+  }
+  if (num1 > num2) {
+    const min = num1;
+    num1 = num2;
+    num2 = min;
+  }
+  let result = 0;
+  for (let i = num1; i <= num2; i++) {
+    result += num1;
+    num1++;
+  }
+  return result;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+ 
