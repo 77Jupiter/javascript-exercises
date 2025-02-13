9095a3dac487fc79f7f693ff3828be89531adff6
Cameron Brown <moontsukuyomi6@gmail.com>
2025-02-13 15:29:30 +0200

    Complete exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let firstValue = 1;
+  let secondValue = 0;
+  let sum = 0;
+  if (number < 0) return 'OOPS';
 
+  for (let i = 0; i < number; i++) {
+    sum = firstValue + secondValue;
+    firstValue = secondValue;
+    secondValue = sum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
